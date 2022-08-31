ae40666dcb06008eef63ae67deee68365a95e58d
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-08-31 10:37:41 +0000

    Add structure in comments of game

--- run.py
+++ run.py
@@ -2,3 +2,22 @@
 
 import random
 # Allows us to call the randint() function
+
+# Game instruction text
+
+# Enter name input
+
+# random number generated
+
+# Player chooses number
+
+# Validator for numbers
+# Numbers out of range
+# Not a number entered
+
+# Number in range but incorrect
+# Message stating too high/low, guess again
+# Guesses left text
+
+
+# Number correct, Congratulations message
